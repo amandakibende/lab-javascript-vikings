2ea9194f85aa48a693d95a98a4587ed9ce0a1eb5
Amanda KIBENDE <contactproamanda@gmail.com>
2021-07-06 15:26:07 +0200

    iteration 1

--- src/viking.js
+++ src/viking.js
@@ -1,5 +1,23 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor (health, strength) {
+    this.health = health
+    this.strength = strength
+  }
+
+attack (){
+  return this.strength;
+}
+
+receiveDamage(damage){
+  this.health = this.health - damage; 
+  if (this.health <=0){
+
+    console.log(`${this.health} W AS T E D`)
+  }
+}
+
+}
 
 // Viking
 class Viking {}
@@ -12,8 +30,13 @@ class War {}
 
 
 
+
+
+
+
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
   module.exports = { Soldier, Viking, Saxon, War };
 }
+
