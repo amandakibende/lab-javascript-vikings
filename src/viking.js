6479610b5a17f1a5b8ff508d9edfb7bc781f0ab8
Amanda KIBENDE <contactproamanda@gmail.com>
2021-07-06 16:04:43 +0200

    player name

--- src/viking.js
+++ src/viking.js
@@ -1,3 +1,13 @@
+const Player1 = 
+{name: 'Paul',
+health : 100,
+strength : 50,
+}
+
+
+
+
+
 // Soldier
 class Soldier {
   constructor (health, strength) {
@@ -20,9 +30,24 @@ receiveDamage(damage){
 
 // Viking
 class Viking extends Soldier {
-  constructor (name){
+  constructor (name, health, strength){
   super(name);
+  this.name = name
+  this.health = health
+  this.strength = strength
   }
+
+attack(){
+  return this.strength;
+}
+
+receiveDamage(damage){
+  this.health = this.health - damage;
+  if (this.health <=0){
+    console.log(`${this.name} received ${damage} points of damage`);
+  }
+}
+
 }
 
 // Saxon
